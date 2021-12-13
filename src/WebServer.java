1bea6b35afd6cf2e99af7a34ebc97b7eef8520f2
Raluca <carare.raluca27@gmail.com>
2021-12-13 22:35:50 +0200

    Added web server and tests

---
+++ src/WebServer.java
@@ -0,0 +1,218 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class WebServer extends Thread {
+    private int port;
+    private String home;
+    private String status;
+
+
+    public WebServer(final int port, final String home, final String status) {
+        this.port = port;
+        this.home = home;
+        this.status = status;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(final int port) {
+        this.port = port;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(final String home) {
+        this.home = home;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(final String status) {
+        this.status = status;
+    }
+
+    public ServerSocket createServerSocket(final int port) throws IllegalArgumentException, BindException {
+        try {
+            ServerSocket socket = new ServerSocket(port);
+            this.setPort(port);
+            System.out.println("Created server socket on port: " + port);
+            return socket;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println("Port is out of range.");
+            throw illegalArgumentException;
+        } catch (BindException bindException) {
+            System.out.println("Port already occupied.");
+            throw bindException;
+        } catch (Exception e) {
+            System.out.println("Failed creating server socket on port: " + port);
+            System.out.println("Exception: " + e);
+            return null;
+        }
+    }
+
+    public void closeServerSocket(final ServerSocket socket) throws NullPointerException {
+        try {
+            socket.close();
+            System.out.println("Closed server socket on port: " + socket.getLocalPort());
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Socket is Null.");
+            throw nullPointerException;
+        } catch (Exception e) {
+            System.out.println("Error closing server socket on port: " + socket.getLocalPort());
+            System.out.println("Exception: " + e);
+        }
+    }
+
+    public Socket acceptConnectedSocket(final ServerSocket socket) throws Exception {
+        try {
+            return socket.accept();
+        } catch (Exception e) {
+            System.out.println("Error accepting connected socket.");
+            System.out.println("Exception: " + e);
+            throw e;
+        }
+    }
+
+    public void closeAcceptedSocket(final Socket acceptedSocket) throws Exception {
+        try {
+            acceptedSocket.close();
+
+        } catch (Exception e) {
+            System.out.println("Error closing accepted socket.");
+            System.out.println("Exception: " + e);
+            throw e;
+        }
+    }
+
+    public ArrayList<String> readInputStream(final Socket acceptedSocket) throws Exception {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(acceptedSocket.getInputStream()));
+            ArrayList<String> inputStream = new ArrayList<>();
+            String s;
+            while ((s = in.readLine()) != null) {
+                inputStream.add(s);
+                if (s.trim().equals("")) {
+                    break;
+                }
+            }
+            return inputStream;
+        } catch (Exception e) {
+            System.out.println("Error reading input stream");
+            System.out.println("Exception: " + e);
+            throw e;
+        }
+    }
+
+    public void sendOutputStream(final Socket acceptedSocket, final Path filePath, final String version) throws Exception {
+        try {
+            OutputStream out = acceptedSocket.getOutputStream();
+            String status = this.getStatus();
+            switch (status) {
+                case "Running": {
+                    if (Files.exists(filePath)) {
+                        String contentType = Files.probeContentType(filePath);
+                        byte[] fileContent = Files.readAllBytes(filePath);
+                        out.write((version + " \r\n200 OK").getBytes());
+                        out.write(("ContentType: " + contentType + "\r\n").getBytes());
+                        out.write("\r\n".getBytes());
+                        out.write(fileContent);
+                        out.write("\r\n\r\n".getBytes());
+                    } else {
+                        String contentType = Files.probeContentType(Paths.get(this.getHome(), "404.html"));
+                        byte[] fileContent = Files.readAllBytes(Paths.get(this.getHome(), "404.html"));
+                        out.write((version + " \r\n404 Not Found").getBytes());
+                        out.write(("ContentType: " + contentType + "\r\n").getBytes());
+                        out.write("\r\n".getBytes());
+                        out.write(fileContent);
+                        out.write("\r\n\r\n".getBytes());
+                    }
+                }
+                case "Maintenance": {
+                    String contentType = Files.probeContentType(Paths.get(this.getHome(), "Maintenance.html"));
+                    byte[] fileContent = Files.readAllBytes(Paths.get(this.getHome(), "Maintenance.html"));
+                    out.write((version + " \r\n503 Service Unavailable").getBytes());
+                    out.write(("ContentType: " + contentType + "\r\n").getBytes());
+                    out.write("\r\n".getBytes());
+                    out.write(fileContent);
+                    out.write("\r\n\r\n".getBytes());
+                }
+                case "Stopped": {
+                    System.out.println("Request made while Web Server is stopped.");
+                }
+                default: {
+                    System.out.println("The current Web Server status is not defined.");
+                }
+            }
+            out.close();
+
+        } catch (Exception e) {
+            System.out.println("Error sending output stream.");
+            System.out.println("Exception: " + e);
+            throw e;
+        }
+    }
+
+    public void handleRequest() throws Exception {
+        try {
+            ServerSocket socket = createServerSocket(this.getPort());
+            Socket acceptedSocket = acceptConnectedSocket(socket);
+            ArrayList<String> inputStream = readInputStream(acceptedSocket);
+            if (inputStream.size() != 0) {
+                String path = inputStream.get(0).split(" ")[1];
+                if (path.equals("/")) {
+                    path = "/index.html";
+                }
+                try {
+                    path = this.getHome() + path;
+                } catch (NullPointerException nullPointerException) {
+                    this.closeAcceptedSocket(acceptedSocket);
+                    this.closeServerSocket(socket);
+                    throw nullPointerException;
+                }
+                Path filePath = Paths.get(path);
+                String version = inputStream.get(0).split(" ")[2];
+                sendOutputStream(acceptedSocket, filePath, version);
+            }
+            this.closeServerSocket(socket);
+            this.closeAcceptedSocket(acceptedSocket);
+        } catch (Exception e) {
+            System.out.println("Error handling request.");
+            System.out.println("Exception: " + e);
+            throw e;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        WebServer Server = new WebServer(8080,
+                "C:/Users/ADMIN/Desktop/WebPages",
+                "Running");
+        for (;;) {
+            try {
+                Server.handleRequest();
+                if (args.length > 0) {
+                    if (args[0].equals("Test")) {
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
+        }
+    }
+}
---
+++ src/WebServerTest.java
@@ -0,0 +1,275 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class WebServerTest {
+
+    private int port = 8080;
+
+    @Test
+    public void setPort_GetPort() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Server.setPort(0);
+        Assertions.assertEquals(0,Server.getPort());
+    }
+
+    @Test
+    public void setHome_GetHome() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Server.setHome("C:/Users/ADMIN/Desktop/WebPages/ASDF");
+        Assertions.assertEquals("C:/Users/ADMIN/Desktop/WebPages/ASDF", Server.getHome());
+    }
+
+    @Test
+    public void setStatus_GetStatus() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Server.setStatus("Stopped");
+        Assertions.assertEquals("Stopped", Server.getStatus());
+    }
+
+    @Test
+    public void createServerSocket_Success() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+
+        try {
+            ServerSocket socket = Server.createServerSocket(port);
+
+            Assertions.assertTrue(socket.isBound());
+
+            socket.close();
+        }catch(BindException bindException) {
+            Assertions.fail(bindException);
+        }catch(IOException ioException) {
+            Assertions.fail(ioException);
+        }
+    }
+
+    @Test
+    public void createServerSocket_PortOutOfRange_ThrowsIllegalArgumentException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(IllegalArgumentException.class,() -> {
+            ServerSocket socket = Server.createServerSocket(-1);
+        });
+    }
+
+    @Test
+    public void createServerSocket_PortOccupied_ThrowsBindException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(BindException.class,() -> {
+            ServerSocket socket1 = Server.createServerSocket(port);
+            ServerSocket socket2 = Server.createServerSocket(port);
+        });
+    }
+
+    @Test
+    public void closeServerSocket_Success() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            ServerSocket socket = Server.createServerSocket(port);
+            Server.closeServerSocket(socket);
+            Assertions.assertTrue(socket.isClosed());
+        }catch(BindException bindException) {
+            Assertions.fail(bindException);
+        }
+    }
+
+    @Test
+    public void closeServerSocket_NullSocket_ThrowsNullPointerException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(NullPointerException.class,() -> {
+            ServerSocket socket = null;
+            Server.closeServerSocket(socket);
+        });
+    }
+
+    @Test
+    public void acceptConnectedSocket_Success() {
+        /* Connect to localhost:8080 when running this test */
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            ServerSocket socket = Server.createServerSocket(port);
+            Socket acceptedSocket = Server.acceptConnectedSocket(socket);
+
+            Assertions.assertTrue(acceptedSocket.isBound());
+
+            socket.close();
+            acceptedSocket.close();
+        }catch(BindException bindException) {
+            Assertions.fail(bindException);
+        }catch(IOException ioException) {
+            Assertions.fail(ioException);
+        }catch(Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void acceptConnectedSocket_AcceptFromClosedServerSocket_ThrowsSocketException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(SocketException.class,() -> {
+            ServerSocket socket = Server.createServerSocket(port);
+            socket.close();
+            Socket acceptedSocket = Server.acceptConnectedSocket(socket);
+            acceptedSocket.close();
+        });
+    }
+
+    @Test
+    public void closeAcceptedSocket_Success() {
+        /* Connect to localhost:8080 when running this test */
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            ServerSocket socket = Server.createServerSocket(port);
+            Socket acceptedSocket = Server.acceptConnectedSocket(socket);
+            Server.closeAcceptedSocket(acceptedSocket);
+            Assertions.assertTrue(acceptedSocket.isClosed());
+
+            socket.close();
+        }catch(BindException bindException) {
+            Assertions.fail(bindException);
+        }catch(IOException ioException) {
+            Assertions.fail(ioException);
+        }catch(Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void closeAcceptedSocket_NullSocket_ThrowsNullPointerException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(NullPointerException.class,() -> {
+            Socket acceptedSocket = null;
+            Server.closeAcceptedSocket(acceptedSocket);
+        });
+    }
+
+    @Test
+    public void readInputStream_Success() {
+        /* Connect to localhost:8080 when running this test */
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            ServerSocket socket = Server.createServerSocket(port);
+            Socket acceptedSocket = Server.acceptConnectedSocket(socket);
+            ArrayList<String> inputStream = null;
+            inputStream = Server.readInputStream(acceptedSocket);
+            Assertions.assertFalse(inputStream.isEmpty());
+            acceptedSocket.close();
+            socket.close();
+        }catch(BindException bindException) {
+            Assertions.fail(bindException);
+        }catch(IOException ioException) {
+            Assertions.fail(ioException);
+        }catch(Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void readInputStream_NullSocket_ThrowsNullPointerException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(NullPointerException.class,() -> {
+            Socket acceptedSocket = null;
+            Server.readInputStream(acceptedSocket);
+        });
+    }
+
+    @Test
+    public void sendOutputStream_Success_ServerRunning() {
+        /* Connect to localhost:8082 when running this test */
+        WebServer Server = new WebServer(8082, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            ServerSocket socket = Server.createServerSocket(8082);
+            Socket acceptedSocket = Server.acceptConnectedSocket(socket);
+            Server.sendOutputStream(acceptedSocket, Paths.get("C:/Users/ADMIN/Desktop/WebPages/a.html"),"HTTP/1.1");
+
+            acceptedSocket.close();
+            socket.close();
+        }catch(BindException bindException) {
+            Assertions.fail(bindException);
+        }catch(IOException ioException) {
+            Assertions.fail(ioException);
+        }catch(Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void sendOutputStream_NullSocket_ThrowsNullPointerException() {
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        Assertions.assertThrows(NullPointerException.class,() -> {
+            Socket acceptedSocket = null;
+            Server.sendOutputStream(acceptedSocket, Paths.get("C:/Users/ADMIN/Desktop/WebPages/a.html"),"HTTP/1.1");
+
+        });
+    }
+
+    @Test
+    public void handleRequest_Success_ServerRunning() {
+        /* Connect to localhost:8080 when running this test */
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            Server.handleRequest();
+        }catch(Exception e)
+        {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void handleRequest_Success_ServerRunning_FileNotFound() {
+        /* Connect to localhost:8080/asdf.html when running this test */
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Running");
+        try {
+            Server.handleRequest();
+        }catch(Exception e)
+        {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void handleRequest_Success_ServerMaintenance() {
+        /* Connect to localhost:8080 when running this test */
+        WebServer Server = new WebServer(port, "C:/Users/ADMIN/Desktop/WebPages", "Maintenance");
+        try {
+            Server.handleRequest();
+        }catch(Exception e)
+        {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void handleRequest_Success_ServerStopped() {
+        /* Connect to localhost:8081 when running this test */
+        WebServer Server = new WebServer(8081, "C:/Users/ADMIN/Desktop/WebPages", "Stopped");
+        try {
+            Server.handleRequest();
+        }catch(Exception e)
+        {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void handleRequest_NullHome_ThrowsNoSuchFileException() {
+        /* Connect to localhost:8081 when running this test */
+        WebServer Server = new WebServer(8081, null, "Running");
+        Assertions.assertThrows(NoSuchFileException.class,() -> {
+            Server.handleRequest();
+        });
+    }
+
+    @Test
+    public void main() {
+        Main main = new Main();
+        main.main(new String[] {"Test"});
+    }
+}
